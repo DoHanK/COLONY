62d98e75beb822c624e4e8f5c1b7ad6906920369
seojin <112840742+seojin3482@users.noreply.github.com>
2024-05-12 19:56:15 +0900

    이펙트 텍스처만 변경 ..

--- 게임개발/Colony/Colony/ColonyScene.cpp
+++ 게임개발/Colony/Colony/ColonyScene.cpp
@@ -543,13 +543,12 @@ void GamePlayScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommand
 
 	//billboard test
 	m_pBillObject = new Billboard(pd3dDevice, pd3dCommandList, pd3dGraphicsRootSignature,
-	pResourceManager->BringTexture("Model/Textures/shootEffect.dds", BILLBOARD_TEXTURE, true), m_BillShader,m_pPlayer->m_SelectWeapon.FindFrame("Export"));
+	pResourceManager->BringTexture("Model/Textures/Explosion03.dds", BILLBOARD_TEXTURE, true), m_BillShader,m_pPlayer->m_SelectWeapon.FindFrame("Export"));
 	m_pPlayer->m_pExportEffect = m_pBillObject;
 	m_pBillObject->doAnimate = true;
-	//m_pBillObject->SetAddPosition(XMFLOAT3(0.0f, 0.3f,0.0f));
-	m_pBillObject->m_OffsetPos = XMFLOAT3(0.0f, 0.0f, 0.0f);
-	m_pBillObject->SetRowNCol(7, 5);
-	m_pBillObject->m_BillMesh->UpdataVertexPosition(UIRect(0.1, -0.1, -0.1, 0.1), 1.0f);
+	m_pBillObject->active = false;
+	m_pBillObject->SetRowNCol(4, 4);
+	m_pBillObject->m_BillMesh->UpdataVertexPosition(UIRect(0.15, -0.15, -0.15, 0.15), 1.0f);
 	m_pBillObject->m_BillMesh->UpdateUvCoord(UIRect(1, 0, 0, 1));
 	m_pBillObject->SettedTimer = 0.01f;
 	m_pBillObject->doOnce = true;
